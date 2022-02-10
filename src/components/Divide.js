2ea560661f5abb0c2515557c4fc1023a39ab9d24
qhtjd2131 <qhtjd2131@gmail.com>
2022-02-10 17:20:21 +0900

    change divide number

--- src/components/Divide.js
+++ src/components/Divide.js
@@ -3,7 +3,7 @@ import React from "react";
 const Divide = () => {
   return (
     <div className="calc-wrapper">
-      <div className="number-square">8</div>
+      <div className="number-square">80</div>
       <div className="sign-square">/</div>
       <div className="number-square">2</div>
     </div>
