37792d1beafbb743179c58bc6fadf6dddf840a73
qhtjd2131 <qhtjd2131@gmail.com>
2022-02-10 17:04:53 +0900

    calcMultiple branch

---
+++ src/components/Multiple.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Multiple = () => {
+
+    return (
+        <div className="calc-wrapper">
+            <div className="number-square">
+                5
+            </div>
+            <div className="sign-square">
+                +
+            </div>
+            <div className="number-square">
+                2
+            </div>
+        </div>
+
+    )
+}
+
+export default Multiple;
