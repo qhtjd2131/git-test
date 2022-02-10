c56bf7ac6cf528960a65e662ef9f66bb5493f060
qhtjd2131 <qhtjd2131@gmail.com>
2022-02-10 17:46:00 +0900

    minus branch 숫자 변경

--- src/components/Minus.js
+++ src/components/Minus.js
@@ -3,9 +3,9 @@ import React from "react";
 const Minus = () => {
   return (
     <div className="calc-wrapper">
-      <div className="number-square">5</div>
+      <div className="number-square">15</div>
       <div className="sign-square">-</div>
-      <div className="number-square">2</div>
+      <div className="number-square">8</div>
     </div>
   );
 };
